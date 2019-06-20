b5326d30fac3c6e848b00517ba69d598254cdda6
Donny Layug <donn.layug@yahoo.com>
2019-06-20 17:13:27 +0800

    First official commit. Finished creating the home page.

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var babel       = require('gulp-babel');
+var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js/bootstrap"))
+        .pipe(browserSync.stream());
+});
+
+// Copy files into dist folder
+gulp.task('copy', function() {
+    return gulp.src([
+        'src/*.html',
+        'src/**/*.jpg',
+        'src/**/*.png',
+        'src/**/*.svg',
+        'src/**/*.css',
+        'src/**/**/*.**.js'
+    ])
+    .pipe(gulp.dest('dist'));
+});
+
+// Process javascript into babel
+gulp.task('processJs', function() {
+    return gulp.src('src/js/*.js')
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(gulp.dest('dist/js'));
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series('sass', function() {
+
+    browserSync.init({
+        server: "./src"  
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
+    gulp.watch(['src/*.html', 'src/*.js']).on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.parallel('js', 'serve', 'copy', 'processJs'));
