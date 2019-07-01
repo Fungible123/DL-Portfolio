cc56a49e56a7d7b9569b5deb1bedff9311017f70
Donny Layug <donn.layug@yahoo.com>
2019-07-01 15:53:40 +0800

    fixed positioning issues in mobile view

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 // Mobile Menu Effect
 var flipMenu = function flipMenu() {
   var arrowMenu = document.querySelector('.mobile-nav');
-  var arrowImage = document.querySelector('.arrow-image');
+  var arrowImage = document.querySelector('.arrow');
   arrowImage.addEventListener('click', function (e) {
     e.preventDefault();
     arrowMenu.classList.toggle('open');
